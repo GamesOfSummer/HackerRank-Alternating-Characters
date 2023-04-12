b0c544f9b2c4036bcdba509fafe03a2073262041
Summer T <summer@summertime-development.com>
2023-04-12 16:57:14 -0500

    progress

--- src/index.ts
+++ src/index.ts
@@ -5,37 +5,25 @@ import {
     validateFxn,
 } from './helpers.js';
 
-function makeAnagram(a: string, b: string): number {
-    let sortedAArray = a.split('');
-    let sortedBArray = b.split('');
+function alternatingCharacters(s: string): number {
+    let sortedAArray = s.split('');
+    let counter = 0;
 
-    let total = sortedAArray.length + sortedBArray.length;
-
-    let pairs = 0;
     for (let i = 0; i < sortedAArray.length; i++) {
-        for (let j = 0; j < sortedBArray.length; j++) {
-            if (sortedAArray[i] === sortedBArray[j]) {
-                sortedBArray.splice(j, 1);
-                pairs++;
-                break;
-            }
+        if (sortedAArray[i] === sortedAArray[i + 1]) {
+            counter++;
+            i++;
         }
     }
 
-    let pairs2 = pairs * 2;
-    return total - pairs2;
+    return counter;
 }
 
 consoleStart();
 
-validateFxn(
-    makeAnagram('fcrxzwscanmligyxyvym', 'jxwtrhvujlmrpdoqbisbwhmgpmeoke'),
-    30
-);
-
-validateFxn(makeAnagram('showman', 'woman'), 2);
-
-validateFxn(makeAnagram('cde', 'abc'), 4);
+validateFxn(alternatingCharacters('AAAA'), 3);
+validateFxn(alternatingCharacters('BBBBB'), 4);
+validateFxn(alternatingCharacters('ABABABAB'), 0);
 
 consoleEnd();
 consoleBuffer();
